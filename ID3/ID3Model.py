a1785c7a678ea849a1f3349786f50bb3dc573504
joker <342028413@qq.com>
2017-12-27 18:35:48 +0800

    ID3 树构建，分类， 信息熵

--- ID3/ID3Model.py
+++ ID3/ID3Model.py
@@ -1,6 +1,7 @@
 from math import log
 import operator
 
+
 def createDataSet():
     dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
@@ -35,6 +36,7 @@ def splitDataSet(dataSet, axis, value):
             retDatSet.append(reduceFeatVec)
     return retDatSet
 
+
 # 定义按照最大信息增益划分数据的函数
 def chooseBestFeatureToSpilt(dateSet):
     numFeature = len(dataSet[0]) - 1  # 特征值个数
@@ -47,7 +49,7 @@ def chooseBestFeatureToSpilt(dateSet):
         newEntropy = 0
         for value in uniqualVals:
             subDataSet = splitDataSet(dataSet, i, value)
-            prob = len(subDataSet)/float(len(dataSet))
+            prob = len(subDataSet) / float(len(dataSet))
             newEntropy += prob * calShannonEnt(subDataSet)  # 对各子集香农熵求和
         infoGain = baseEntropy - newEntropy
         # 最大信息增益
@@ -56,14 +58,17 @@ def chooseBestFeatureToSpilt(dateSet):
             bestFeature = i
     return bestFeature
 
+
 # 投票表决代码
 def majorityCnt(classList):
     classCount = {}
     for vote in classList:
-        if vote not in classCount.keys(): classCount[vote] = 0
+        if vote not in classCount.keys():
+            classCount[vote] = 0
         classCount[vote] += 1
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
 
 def createTree(dataSet, labels):
     classList = [example[-1] for example in dataSet]
@@ -71,27 +76,46 @@ def createTree(dataSet, labels):
     if classList.count(classList[-1]) == len(classList):
         return classList[-1]
     # 长度为1，返回出现次数最多的类别
-    if len(classList[0]) == 1:
+    if len(classList) == 1:
         return majorityCnt(classList)
     # 按照信息增益最高选取分类特征属性
     bestFeat = chooseBestFeatureToSpilt(dataSet)  # 返回分类的特征序号
     bestFeatLable = labels[bestFeat]  # 该特征的label
-    myTree = {bestFeatLable: {}}      # 构建树的字典
+    myTree = {bestFeatLable: {}}  # 构建树的字典
     del (labels[bestFeat])  # 从labels的list中删除该label
     featValues = [example[bestFeat] for example in dataSet]
-    uniqueVals = set(featValues)     # 所有关于该特征的非重复的值（除掉用过的）
+    uniqueVals = set(featValues)  # 所有关于该特征的非重复的值（除掉用过的）
 
     for value in uniqueVals:
-        subLables = labels[:]  # 子集合（除了该特征的所有的特征）
-        myTree[bestFeatLable][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLables)
+        subLabels = labels[:]  # 子集合（除了该特征的所有的特征）
+        myTree[bestFeatLable][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
     return myTree
+
+
+# 决策树运用于分类
+def classify(inputTree, featLables, testVec):
+    # [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    firstStr = list(inputTree.keys())[0]  # 获取树的第一个特征属性
+    secondDict = inputTree[firstStr]  # 树的分支，子集合Dict
+    featIndex = featLables.index(firstStr)  # 获取决策树第一层在featLables中的位置
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLables, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
 
 
 if __name__ == '__main__':
     dataSet, labels = createDataSet()
-    print(dataSet)
-    print(calShannonEnt(dataSet))
-    print(splitDataSet(dataSet, 0, 0))
-    print(splitDataSet(dataSet, 0, 1))
-    print(chooseBestFeatureToSpilt(dataSet))
-    createTree(dataSet, labels)
+    # print(dataSet)
+    # print(labels)
+    # print(calShannonEnt(dataSet))
+    # print(splitDataSet(dataSet, 0, 0))
+    # print(splitDataSet(dataSet, 0, 1))
+    # print(chooseBestFeatureToSpilt(dataSet))
+    # print(createTree(dataSet, labels))
+    trees = createTree(dataSet, labels)
+    dataSet, labels = createDataSet()
+    print(classify(trees, labels, [1, 1]))
